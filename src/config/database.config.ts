02c63243d2c963b1e94f1a6c4fdc45f48e900393
Justxt <jostinmatt@gmail.com>
2025-02-17 22:46:17 -0500

    Proyecto creado y configurada su bd

---
+++ src/config/database.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: true,
+};
+
+export default (): TypeOrmModuleOptions => databaseConfig;
